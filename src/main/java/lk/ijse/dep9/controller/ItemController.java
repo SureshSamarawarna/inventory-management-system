e257779b0dbb0f020a55a0d41b101f35194a72da
SureshSamarawarna <suresh.asan8@gmail.com>
2023-03-04 11:32:56 +0530

    created CRUD operations

--- src/main/java/lk/ijse/dep9/controller/ItemController.java
+++ src/main/java/lk/ijse/dep9/controller/ItemController.java
@@ -1,8 +1,14 @@
 package lk.ijse.dep9.controller;
 
+import lk.ijse.dep9.exception.ResourceNotFoundException;
 import lk.ijse.dep9.model.Item;
 import lk.ijse.dep9.repository.ItemRepository;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.*;
 
 @CrossOrigin
 @RestController
@@ -17,7 +23,53 @@ public class ItemController {
 
     @PostMapping()
     public Item createItems(@RequestBody Item item){
+        System.out.println("request came");
         return itemRepository.save(item);
+    }
+
+    @PutMapping("/{id}")
+    public Item updateItem(@PathVariable int id,@RequestBody Item item){
+        Item foundItem = itemRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Can't fint item with : " + id));
+        foundItem.setName(item.getName());
+        foundItem.setCategory(item.getCategory());
+        foundItem.setDiscount(item.getDiscount());
+        foundItem.setPrice(item.getPrice());
+        foundItem.setProductId(item.getProductId());
+        foundItem.setQty(item.getQty());
+
+        return itemRepository.save(foundItem);
+    }
+
+    @GetMapping
+    public List<Item> getAllItems(){
+        return itemRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Item> getItemById(@PathVariable int id){
+        Item item = itemRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Can,t find item with: "+id));
+        return Optional.of(item);
+    }
+
+    @DeleteMapping("/{id}")
+    public Map<String,Boolean> deleteItemById(@PathVariable int id){
+        Map<String,Boolean> status = new HashMap<>();
+        Item foundItem = itemRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Can,t find item with: "+id));
+        itemRepository.delete(foundItem);
+        status.put("DELETED",Boolean.TRUE);
+        return status;
+
+    }
+
+    @DeleteMapping()
+    public Map<String,Boolean> deleteAllItems(){
+        Map<String,Boolean> status = new HashMap<>();
+        itemRepository.deleteAll();
+        status.put("DELETED ALL ITEMS",Boolean.TRUE);
+        return status;
+
     }
 
 }
--- src/main/java/lk/ijse/dep9/model/Item.java
+++ src/main/java/lk/ijse/dep9/model/Item.java
@@ -1,17 +1,23 @@
 package lk.ijse.dep9.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
 @Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "product_id")
     private int productId;
-    @Column(name = "name")
     private String name;
     private BigDecimal price;
     private int qty;
